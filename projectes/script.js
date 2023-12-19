a9bf62e9aac9048fd0e64452a04bb47aefd4e744
Ch-Tima <chizhevskii.tima@gmail.com>
2023-12-19 19:36:53 +0100

    animation for details

--- projectes/script.js
+++ projectes/script.js
@@ -8,35 +8,42 @@ const list = [
     [
         {
             title: "WebBeautyBook",
-            link: "https://github.com/Ch-Tima/WebBeautyBook"
+            link: "https://github.com/Ch-Tima/WebBeautyBook",
+            img: ""
         },
         {
             title: "CurrencyConverter",
-            link: "https://github.com/Ch-Tima/CurrencyConverter"
+            link: "https://github.com/Ch-Tima/CurrencyConverter",
+            img: "icon_02.png"
         },
         {
             title: "Movie",
-            link: "https://github.com/Ch-Tima/Movie-net-6.0"
+            link: "https://github.com/Ch-Tima/Movie-net-6.0",
+            img: ""
         }
     ], 
     [
         {
             title: "Guard",
-            link: "https://github.com/AN0NCER/guard"
+            link: "https://github.com/AN0NCER/guard",
+            img: ""
         },
         {
             title: "Steam-lib",
-            link: "https://github.com/AN0NCER/steam-lib"
+            link: "https://github.com/AN0NCER/steam-lib",
+            img: ""
         },
         {
             title: "WebAds",
             link: "https://github.com/Ch-Tima/WebAds",
+            img: ""
         }
     ], 
     [
         {
             title: "SpaceInvaders",
-            link: "https://github.com/Ch-Tima/SpaceInvaders"
+            link: "https://github.com/Ch-Tima/SpaceInvaders",
+            img: ""
         }
     ]
 ]
@@ -75,12 +82,28 @@ function addProjectes(qE, arr){
     for (let i = 0; i < arr.length; i++) {
         var radius = qE.width()/2;
         var radians = (((360 / arr.length) * i) * (Math.PI / 180)) + countCirlce
-        let item = $("<a>").text(arr[i].title).attr("href",arr[i].link).addClass("item").css({
+        let item = $("<a>").attr("href",arr[i].link).css({
             'width': itemSize,
             'height': itemSize,
             'left': (radius * Math.cos((radians))) + radius - itemSize / 2,
             'top': (radius * Math.sin(radians)) + radius - itemSize / 2,
+            'position': 'absolute'
         })
+        .append($("<div>").addClass("item").css({
+            "background-image": `url(${arr[i].img})`,
+            "background-size": "90%",
+            "background-position": "center"
+        }))
+        .append($("<div>").addClass("item-details").css({
+            width: `${100}px`,
+            height: `${itemSize-5}px`,
+            left: `${itemSize/2.25}px`,
+        }))
+        .append($("<p>").text(arr[i].title).css({
+            'margin': '10px 0 0 0',
+            'text-align': 'center'
+        }))
+
         qE.append(item)
     }
 }
