b30597f53eef3bb5fa80eff95fe0b55a8dcf7afe
Ch-Tima <chizhevskii.tima@gmail.com>
2023-12-20 20:11:48 +0100

    Added links

--- projectes/script.js
+++ projectes/script.js
@@ -1,49 +1,76 @@
 const container = $('.container');
 const circle = $('#main');
 
-let itemSize = 66;
+let itemSize = 80;
 let countCirlce = 1;
 
 const list = [
     [
+
         {
             title: "WebBeautyBook",
-            link: "https://github.com/Ch-Tima/WebBeautyBook",
+            links:{
+                git: "https://github.com/Ch-Tima/WebBeautyBook",
+                web: "https://appwebbeautybook.azurewebsites.net"
+            },
             img: ""
         },
         {
             title: "CurrencyConverter",
-            link: "https://github.com/Ch-Tima/CurrencyConverter",
-            img: "icon_02.png"
+            links:{
+                git: "https://github.com/Ch-Tima/CurrencyConverter",
+                kofi: "https://ko-fi.com/i/IL4L5LHX6E",
+                playStore: "https://play.google.com/store/apps/details?id=com.chtima.currencyconverter"
+            },
+            img: "img/CC.png"
         },
         {
             title: "Movie",
-            link: "https://github.com/Ch-Tima/Movie-net-6.0",
+            links:{
+                git: "https://github.com/Ch-Tima/Movie-net-6.0",
+            },
             img: ""
+        },
+        {
+            title: "Flashlight",
+            links:{
+                kofi: "https://ko-fi.com/i/IL4L5LHX6E",
+                playStore: "https://play.google.com/store/apps/details?id=com.chtima.flashlight"
+            },
+            img: "img/F.png"
         }
     ], 
     [
         {
             title: "Guard",
-            link: "https://github.com/AN0NCER/guard",
-            img: ""
+            links:{
+                git: "https://github.com/AN0NCER/guard",
+            },
+            img: "img/G.png"
         },
         {
             title: "Steam-lib",
-            link: "https://github.com/AN0NCER/steam-lib",
-            img: ""
+            links:{
+                git: "https://github.com/AN0NCER/steam-lib",
+                nuget: "https://www.nuget.org/packages/SteamAuthStandart"
+            },
+            img: "img/G-lib.png"
         },
         {
             title: "WebAds",
-            link: "https://github.com/Ch-Tima/WebAds",
+            links:{
+                git: "https://github.com/Ch-Tima/WebAds",
+            },
             img: ""
         }
     ], 
     [
         {
             title: "SpaceInvaders",
-            link: "https://github.com/Ch-Tima/SpaceInvaders",
-            img: ""
+            links:{
+                git: "https://github.com/Ch-Tima/SpaceInvaders",
+            },
+            img: "img/SI.png"
         }
     ]
 ]
@@ -71,7 +98,7 @@ function rerender(){
     circle.children().remove();
     countCirlce = 1;
 
-    itemSize = $(window).width() >= 750 ? 66 : ($(window).width() <= 750 && $(window).width() >= 540 ? 50 : 45)
+    itemSize = $(window).width() >= 750 ? 80 : ($(window).width() <= 750 && $(window).width() >= 540 ? 70 : 55)
     
     addProjectes(circle, list[0])
     createCircle(0.68, 1).appendTo(container);
@@ -92,18 +119,39 @@ function addProjectes(qE, arr){
         .append($("<div>").addClass("item").css({
             "background-image": `url(${arr[i].img})`,
             "background-size": "90%",
-            "background-position": "center"
+            "background-position": "center",
+            "background-repeat": "no-repeat"
         }))
-        .append($("<div>").addClass("item-details").css({
-            width: `${100}px`,
-            height: `${itemSize-5}px`,
-            left: `${itemSize/2.25}px`,
-        }))
-        .append($("<p>").text(arr[i].title).css({
-            'margin': '10px 0 0 0',
-            'text-align': 'center'
-        }))
+        .append(createItemDetails(arr[i]))
 
         qE.append(item)
     }
+}
+
+function createItemDetails(data){
+    console.log(data);
+
+    let links = [];
+
+    for (var key in data.links) {
+        console.log(key);
+
+        links.push($("<a>").attr("href", data.links[key]).append($("<img>").attr('src', `../assets/svg/${key}.svg`).css({
+            'width': '25px',
+            'height': '25px',
+            'margin': '0 5px' 
+        })))
+    }
+
+    return $("<div>").addClass("item-details").css({
+        width: `${175}px`,
+        height: `${itemSize-5}px`,
+        left: `${itemSize/2.25}px`,
+    }).append($("<p>").text(data.title).css({
+        'text-align': 'center',
+        'margin': '0'
+    })).append($("<div>").css({
+        'display': 'flex',
+        'justify-content': 'center'
+    }).append(links))
 }
