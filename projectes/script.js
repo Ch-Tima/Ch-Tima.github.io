ca3bd330729197cb5d264cb2dfa8bbf678739b21
Ch-Tima <chizhevskii.tima@gmail.com>
2023-12-22 12:16:36 +0100

    Animation via jQuery

--- projectes/script.js
+++ projectes/script.js
@@ -3,6 +3,7 @@ const circle = $('#main');
 
 let itemSize = 80;
 let countCirlce = 1;
+let winWidth = 0;
 
 const list = [
     [
@@ -94,12 +95,17 @@ function createCircle(scale, indexProject){
 }
 
 function rerender(){
+    //cls
     $(".circle:not(#main)").remove();
     circle.children().remove();
+    $("#hugeDetails").remove();
     countCirlce = 1;
 
-    itemSize = $(window).width() >= 750 ? 80 : ($(window).width() <= 750 && $(window).width() >= 540 ? 70 : 55)
+    //calc item size
+    winWidth = $(window).width();
+    itemSize = winWidth >= 750 ? 80 : winWidth <= 750 && winWidth >= 540 ? 70 : 55
     
+    //draw diograna
     addProjectes(circle, list[0])
     createCircle(0.68, 1).appendTo(container);
     createCircle(0.38, 2).appendTo(container);
@@ -121,37 +127,68 @@ function addProjectes(qE, arr){
             "background-size": "90%",
             "background-position": "center",
             "background-repeat": "no-repeat"
-        }))
-        .append(createItemDetails(arr[i]))
+        }).on("mouseenter", () => showDetails(arr[i], item)))
+        .append($("<div>").addClass("item-details").css({
+            width: `${175}px`,
+            height: `${itemSize-5}px`,
+            left: `${itemSize/2.25}px`,
+        }));
 
         qE.append(item)
     }
 }
 
-function createItemDetails(data){
-    console.log(data);
+function showDetails(event, element){
+    console.log(event)
 
     let links = [];
-
-    for (var key in data.links) {
-        console.log(key);
-
-        links.push($("<a>").attr("href", data.links[key]).append($("<img>").attr('src', `../assets/svg/${key}.svg`).css({
-            'width': '25px',
-            'height': '25px',
-            'margin': '0 5px' 
+    for (var key in event.links) {
+        links.push($("<a>").attr("href", event.links[key]).append($("<img>").attr('src', `../assets/svg/${key}.svg`).css({
+            'width': `${winWidth > 800 ? 28 : (winWidth < 800 && winWidth > 500 ? 55 : 35)}px`,
+            'height': 'auto',
+            'margin': `${winWidth < 800 ? 5 : 0}px ${winWidth < 800 ? 10 : 5}px`
         })))
     }
 
-    return $("<div>").addClass("item-details").css({
-        width: `${175}px`,
-        height: `${itemSize-5}px`,
-        left: `${itemSize/2.25}px`,
-    }).append($("<p>").text(data.title).css({
-        'text-align': 'center',
-        'margin': '0'
-    })).append($("<div>").css({
-        'display': 'flex',
-        'justify-content': 'center'
-    }).append(links))
+    if(winWidth < 800){
+        $("#hugeDetails").remove();
+        $("body").append
+        (
+            $("<div>").css({
+                'width': '100%',
+                'height': '13vh',
+                'background': '#89d1d9',
+                'position': 'absolute',
+                'bottom': '0'
+            }).attr("id", "hugeDetails")
+            .append($("<p>").text(event.title).addClass("title-xxl"))
+            .append($("<div>").addClass("df-center").append(links))
+        )
+    }else{
+        let item = $(element).children(".item");
+        let details = $(element).children(".item-details");
+
+        details.children().remove();
+        details.append($("<p>").text(event.title).addClass("text-center")).append($("<div>").addClass("df-center").append(links));
+        
+        item.addClass("item-on");
+        details.addClass("item-details-on");
+
+        // mouseout | mouseover
+        details.on("mouseover",()=> {
+            item.addClass("item-on");
+            details.addClass("item-details-on");
+            details.one("mouseout", ()=>{
+                item.removeClass("item-on");
+                details.removeClass("item-details-on");
+            })
+        });
+
+        $(element).one("mouseout", ()=>{
+            item.removeClass("item-on");
+            details.removeClass("item-details-on")
+        })
+    }
+
+
 }
